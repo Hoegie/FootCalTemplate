2b90e3f88c9886760d62dd27bb1f4cf76ac1c57d
Hoegie <mezenhof8@gmail.com>
2019-03-24 20:06:10 +0100

    nth commit

--- footcalini.js
+++ footcalini.js
@@ -1,9 +1,9 @@
-module.exports.clubID = '#clubid#';
-module.exports.clubName = '#clubname#';
-module.exports.clubBaseNr = '#clubbasenr#';
+module.exports.clubID = '@clubid@';
+module.exports.clubName = '@clubname@';
+module.exports.clubBaseNr = '@clubbasenr@';
 module.exports.apisec = false;
-module.exports.dbname = '#dbname#';
-module.exports.apachedir = '#serverdir#';
-module.exports.apiport = #apiport#;
+module.exports.dbname = '@dbname@';
+module.exports.apachedir = '@serverdir@';
+module.exports.apiport = @apiport@;
 module.exports.serveraddress = "185.224.89.139";
 
